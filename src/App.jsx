2af270665ad6927bd5df6781e438f3bcab9c631f
Zohaib <zohaibsoomro100@gmail.com>
2023-05-31 20:06:41 +0500

    Components Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Shop from './pages/shop/Shop'
-import Cart from './pages/cart/Cart'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Shop from "./pages/shop/Shop";
+import Cart from "./pages/cart/Cart";
+import ShopContextProvider from "./context/ShopContext";
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/'element={<Shop />}/>
-          <Route path='/cart' element = {<Cart />}/>
-
-        </Routes>
-      </Router>
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
